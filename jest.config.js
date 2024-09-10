ef3af97fcea9bbc0d1d88155fcb2ca1698a3f1a0
Sindre Bøyum <sindre.boyum@tietoevry.com>
2024-09-10 22:52:10 +0200

    build: ESModule

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,18 @@
-module.exports = {
+// @ts-check
+/** @type {import('jest').Config} */
+export default {
   clearMocks: true,
   moduleFileExtensions: ["js", "ts"],
   testEnvironment: "node",
   testMatch: ["**/*.test.ts"],
   testRunner: "jest-circus/runner",
   transform: {
-    "^.+\\.ts$": "ts-jest",
+    "^.+\\.ts$": [
+      "ts-jest", { 
+        isolatedModules: true,
+        useESM: true
+      }
+    ]
   },
   verbose: true,
 };
